4b72865e645b7b0ce865df0e8f6078714b670e6b
a7medjamal <ahmedjmal20003@gmail.com>
2025-01-15 13:52:07 +0200

    Trigger request

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_services.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -13,8 +18,9 @@ class SearchView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 14),
         child: Center(
           child: TextField(
-            onSubmitted: (value) {
-              print(value);
+            onSubmitted: (value) async {
+              WeatherModel weatherModel = await WeatherService(Dio()).getCurrentWeather(cityName: value);
+              print(weatherModel.cityName);
             },
             decoration: const InputDecoration(
               labelText: 'City name',
